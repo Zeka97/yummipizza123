7e87f63093d7a73f3e21f920a21dca4df0b8e150
Amel <zekafrombtm@hotmail.com>
2020-07-19 22:13:38 +0200

    Updates

---
+++ precache-manifest.abc64427bed06b7db0843386df4d09c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4aa2ce12b938e45e45b47ae81c5633",
+    "url": "/yummipizza123/index.html"
+  },
+  {
+    "revision": "0b6e0f9228ce27fb6070",
+    "url": "/yummipizza123/static/css/main.dc075d55.chunk.css"
+  },
+  {
+    "revision": "787c17f42048723d2a36",
+    "url": "/yummipizza123/static/js/2.543db9a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yummipizza123/static/js/2.543db9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6e0f9228ce27fb6070",
+    "url": "/yummipizza123/static/js/main.5e744112.chunk.js"
+  },
+  {
+    "revision": "44e7b9de844906693488",
+    "url": "/yummipizza123/static/js/runtime-main.6968f67b.js"
+  },
+  {
+    "revision": "f5a684b011134debc9ca3d8838713ba1",
+    "url": "/yummipizza123/static/media/Supreme_pizza.f5a684b0.jpg"
+  },
+  {
+    "revision": "4f102d02213b7552c9ed86801e49868a",
+    "url": "/yummipizza123/static/media/displaypicture.4f102d02.png"
+  }
+]);
